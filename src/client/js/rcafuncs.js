3e25ed9e801bfb663900fe150df6817dc04e9649
Bob Anzlovar <rcanzlovar@gmail.com>
2018-03-04 12:05:01 -0700

    more fun with buttons

--- src/client/js/rcafuncs.js
+++ src/client/js/rcafuncs.js
@@ -1,109 +1,123 @@
 // Main Site Funcs
-var API = 'http://192.168.23.18/rtd-routes/api-trips.php?route=BOLT';
+var API = 'http://192.168.23.18/rtd-routes/api-trips.php';
 
 
-//API = 'http://192.168.20.101/rtd-routes/api-trips.php?route=BOLT';
-
+//API = 'http://192.168.20.101/rtd-routes/api-trips.php';
 
 
 
 
 function apitrips(arrayin)
 {
-	var API = 'http://192.168.23.18/rtd-routes/api-trips.php?route=BOLT';
+	var API = 'http://192.168.23.18/rtd-routes/api-trips.php';
+	alert ('type: ' + typeof arrayin);
+	alert ('ting? ');
+	console.log ('arrayin type: ' + typeof arrayin);
 	if (typeof arrayin == 'undefined' ) { //()
 		var route_id = "BOLT";
 		API += "?route=" + route_id;
-	} else if (typeof arrayin !== 'string') { //("thing")
+	} else if (typeof arrayin == 'string') { //("thing")
 		var route_id=arrayin.route
 		API += "?route=" + route_id;
+	} else if ( typeof arrayin.route == 'array') { //({thing:"val"})
+	    alert("got an array");
+	    var route_list1 = ''; 
+	    var route_list2 = ''; 
+	    var thing;
+	    for (i = 0;i < arrayin.length; i++) {
+	    	route_list1 += arrayin[i] + ",";
+	    	route_list2 += "\"" + arrayin[i] + "\",";
+	    }
+	    //substitute out trailing comma
+        route_list1 = route_list1.replace(/,\s*$/, "");
+        route_list2 = route_list2.replace(/,\s*$/, "");
+	    API += "?rlist=" . route_list1;
+	    alert("got an array" + API);
 	} else if ( typeof arrayin.route == 'object') { //({thing:"val"})
+	    alert("got an object");
 		var route_id=arrayin.route
 		API += "?route=" + route_id;
 	}	
 	//  we can get slick here if we want - if it's all numeric and a certain length
 	// then we can assume its a trip
 	// eles its a route  - might be able to check if a route with a call 
+}
 
+function after_apitrips(arrayin)
+{
 
 	var xmlhttp = new XMLHttpRequest();
 	xmlhttp.onreadystatechange = function() {
 	if (this.readyState == 4 && this.status == 200) {
 		var myObj = JSON.parse(this.responseText);
 
+		var i, j = '';
 		var x,y,z; // some collector varia:write
-
 		var h;  //headers
 
-        //count how many times it changes
-		var props = {};
 
 
 
-		var i, j = '';
-//		var myArr = myObj.pools;
 		x = "<ul>";
 		var _tripId = '';
 		var _arrival_time_end = '';
 		var _arrival_time_begin = '';
 		var _route_id = '';
 		for (i in myObj) {
+			// use locObj for simpler referential syntax 
 			locObj = myObj[i];		
-			if (_tripId != locObj.trip_id) {
-				// get the last one, or blank
-				_arrival_time_end = props["arrival_time"] || '';
-				_route_short_name = props["route_short_name"] || '';
-				_route_long_name = props["route_long_name"] || '';
-				_route_id = props["route_id"] || '';
-				console.log("props before" + props);
 
-				z += "<h3>" + _route_id + "</h3>";
-				z += "<h4>" + _route_long_name	 + "</h4>";
+			if (_tripId != locObj.trip_id) {
+
+			    _route_short_name = locObj["route_short_name"] || '';
+			    _route_long_name = locObj["route_long_name"] || '';
+			    _route_id = locObj["route_id"] || '';
+			    z += "<span class='route_name'>" + _route_id + "</span>";
+			    z += "<span class='route_dest'>" + _route_long_name	 + "</span>";
+
+				// get the last one, or blank
 				z += _arrival_time_begin	+ " - " + _arrival_time_end;
 				z += "<a href=\"?trip=" + _tripId + "\">detail</a>";
 
 
-				_tripId = myObj[i].trip_id;
-				_arrival_time_begin = myObj[i].arrival_time;
+//set up for the last iteration of this
+				_tripId = locObj.trip_id;
+				_arrival_time_end = locObj.arrival_time;
 
 			}
 	
 
 		// now let's get the things that didn't change at all 
-		y += 'props<ul>';
-		for (var prop in props) {
-			if (props[prop] != '') {
-				y += '<li>' + prop + " " + props[prop] + '</li>';
-			}
-		}
-		y += '</ul>';
 		//
   			console.log(locObj.trip_id);
 			x += '<li>' + locObj.trip_id + ' ' + myObj[i].stop_name;
 			x += '<br>'
 			x += '</li>';
 			console.log(locObj);
+
+			// stash the values of this locObj into props so we have the last record 
+			// needed for getting the last arrival time in a trip before starting the next one
   			for(var prop in locObj) {
-//  				if (!(prop in props)) {
-  					props[prop] = locObj[prop];
-//  				}
+  				props[prop] = locObj[prop];
 			}
 		}//["0"].trip_id
 		x += "</ul>"
 	
 
-		// // now let's get the things that didn't change at all 
-		// y += '<ul>';
-		// for (var prop in props) {
-		// 	if (props[prop] != '') {
-		// 		y += '<li>' + prop + " " + props[prop] + '</li>';
-		// 	}
-		// }
-		// y += '</ul>';
-		//##################
+
+
+		// y is for debugging purposes, shows the whole thing 
+		y += '<ul>';
+		for (var prop in props) {
+			if (props[prop] != '') {
+				y += '<li>' + prop + " " + props[prop] + '</li>';
+			}
+		}
+		y += '</ul>';
+		
 	
 		document.getElementById("main1").innerHTML = z;
-		document.getElementById("main2").innerHTML = y;
+		document.getElementById("main2").innerHTML = x;
 	}
 }
 xmlhttp.open("GET", API, true);
