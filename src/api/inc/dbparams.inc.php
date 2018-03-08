d50c1c5b9b3e2fb0b3db94ecb60ee3c72a714608
Bob Anzlovar <rcanzlovar@gmail.com>
2018-03-07 20:57:20 -0700

    changing the api-trips to index.php to simplify URL

---
+++ src/api/inc/dbparams.inc.php
@@ -0,0 +1,14 @@
+<?php //inc/dbparams.inc.php
+
+$username = "anzlovar_rtd";
+$password = "92a)=1}F%?77";
+$dbhost = "localhost";
+$dbname = 'anzlovar_rtd-transit';
+
+$username = "root";
+$password = "wordpass123";
+
+$dbhost = "localhost";
+$dbname = 'wmata_gtfs';
+
+?>
