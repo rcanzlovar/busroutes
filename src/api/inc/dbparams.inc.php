27f2aeaa5613c7abbeadd8831252812a0ae89765
bob anzlovar <rcanzlovar@gmail.com>
2018-04-07 15:26:25 -0600

    removed pro password, renamed database

--- src/api/inc/dbparams.inc.php
+++ src/api/inc/dbparams.inc.php
@@ -1,14 +1,10 @@
 <?php //inc/dbparams.inc.php
 
-$username = "anzlovar_rtd";
-$password = "92a)=1}F%?77";
-$dbhost = "localhost";
-$dbname = 'anzlovar_rtd-transit';
 
 $username = "root";
 $password = "wordpass123";
 
 $dbhost = "localhost";
-$dbname = 'wmata_gtfs';
+$dbname = 'gtfs_rtd';
 
 ?>
